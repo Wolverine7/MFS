8cb4761ed66eb9710048c43085c011bdd414de60
bobo <bisamov@gmail.com>
2019-02-04 22:46:25 -0600

    heroku deployment

--- account/urls.py
+++ account/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 
 
-app_name = 'account'
+# app_name = 'account'
 urlpatterns = [
     # path('login/', views.LoginView.as_view(), name='login'),
     #     # path('logout/', views.LogoutView.as_view(), name='logout'),
